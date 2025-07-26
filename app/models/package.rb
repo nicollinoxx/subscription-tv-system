5f7c5db8662c56592f252a8a17d930556a77386c
Nícolas Amaral <nicolas.almeida@aluno.unievangelica.edu.br>
2025-07-26 16:40:45 -0300

    Add package validation and price calculation rules

--- app/models/package.rb
+++ app/models/package.rb
@@ -3,6 +3,18 @@ class Package < ApplicationRecord
 
   belongs_to :plan
 
-  validates :name, :price, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+  validate  :should_have_at_least_one_additional_service
+  before_save :calculate_total_items, unless: -> { price.present? }
+
+  private
+
+    def should_have_at_least_one_additional_service
+      errors.add(:base, "Package must have at least one additional service") unless additional_services.any?
+    end
+
+    def calculate_total_items
+      self.price = (plan.price + additional_services.sum(:price))
+    end
 end
