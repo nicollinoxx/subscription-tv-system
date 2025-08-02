67a94a3b1fb3219a2bf93d6177f5cf8cc8bf0eaf
Nícolas Amaral <nicolas.almeida@aluno.unievangelica.edu.br>
2025-08-02 20:26:41 -0300

    Simplify invoice price calculation logic
    This combines two methods into one private method and removes an
    unneeded method call.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,14 +1,11 @@
 class Invoice < ApplicationRecord
   belongs_to :signature
-  before_save :set_total_price
 
-  def set_total_price
-    self.price = total_price
-  end
+  before_save :set_total_price
 
   private
 
-    def total_price
-      (signature.plan.price || signature.package.price) + signature&.additional_services.sum(:price)
+    def set_total_price
+      self.price = (signature.plan.price || signature.package.price) + signature&.additional_services.sum(:price)
     end
 end
